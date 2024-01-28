33e2742e5ef207b2e8d5b775051d82cb14bed4b1
M. Ahmad <MAhmad@Workstation>
2024-01-28 20:49:51 +0500

    added copyright symbol

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react"
-import Header from "./Components/Header"
-import Body from "./Components/Body"
-import Footer from "./Components/Footer"
+import React from "react";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
 function App() {
-	return (
-		<div className="flex flex-col">
-			<Header/>
-			<Body/>
-			<Footer/>
-		</div>
-	)
+  return (
+    <div className="flex flex-col">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,32 +1,39 @@
 import { useState } from "react";
-import { IoLogoGithub } from "react-icons/io"; 
+import { IoLogoGithub } from "react-icons/io";
 export default function Footer() {
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
   return (
-    <div className='flex bg-black flex-col w-full h-fit pt-10 pb-5 text-white justify-center items-center'>
+    <div className="flex bg-black flex-col w-full h-fit pt-10 pb-5 text-white justify-center items-center">
       <div className="flex flex-col gap-3 justify-center items-center text-center">
         <h1 className="text-3xl">SCHEDULE GENERATOR</h1>
-        <code><p>THIS WAS MADE TO AID ALL THOSE WHO HAVE DIFFERENT CLASSES IN DIFFERENT SECTIONS + NEED TO MAKE TIMETABLES EVERY SEMESTER ON SOME SPREADSHEET</p></code>
+        <code>
+          <p>
+            THIS WAS MADE TO AID ALL THOSE WHO HAVE DIFFERENT CLASSES IN
+            DIFFERENT SECTIONS + NEED TO MAKE TIMETABLES EVERY SEMESTER ON SOME
+            SPREADSHEET
+          </p>
+        </code>
       </div>
       <div className="flex flex-col justify-center items-center p-3 gap-3 ">
-        <code className="text-white"><b>{"Copyright " + currentYear}</b></code>
+        <code className="text-white">
+          <b>Copyright &#169;{currentYear}</b>
+        </code>
         <div className="flex flex-col gap-2 items-center">
-            <h3>MADE WITH LOVE</h3>
-            <div className="flex flex-row gap-2 justify-center align-middle">
-                <IoLogoGithub size={20}/>
-              <a href = "https://github.com/DaniyalFaraz2003">DANIYAL FARAZ</a>
-            </div>
-            <div className="flex flex-row gap-2 justify-center align-middle">
-            <IoLogoGithub size={20}/>
-                <a href="https://github.com/d4niy4l">DANIYAL AHMED</a>
-            </div>
-            <div className="flex flex-row gap-2 justify-center align-middle">
-            <IoLogoGithub size={20}/>
-                 <a href = "https://github.com/muhammadahmadazam">MUHAMMAD AHMAD</a>
-            </div>
+          <h3>MADE WITH LOVE</h3>
+          <div className="flex flex-row gap-2 justify-center align-middle">
+            <IoLogoGithub size={20} />
+            <a href="https://github.com/DaniyalFaraz2003">DANIYAL FARAZ</a>
+          </div>
+          <div className="flex flex-row gap-2 justify-center align-middle">
+            <IoLogoGithub size={20} />
+            <a href="https://github.com/d4niy4l">DANIYAL AHMED</a>
+          </div>
+          <div className="flex flex-row gap-2 justify-center align-middle">
+            <IoLogoGithub size={20} />
+            <a href="https://github.com/muhammadahmadazam">MUHAMMAD AHMAD</a>
+          </div>
         </div>
-      
       </div>
     </div>
   );
