ff065a43578e0c6d6dede8462ce207b91302ddfb
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-02-02 19:20:31 +0500

    google apis

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { google } = require("googleapis");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +18,39 @@ app.use(cors());
 app.get("/home", (req, res) => {
     res.send("Hello World");
 })
+
+// test code for the google sheets api
+app.get("/", async (req, res) => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "key.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    })
+
+    // create client instance for auth
+    const client = await auth.getClient();
+
+    //instance of google sheets api
+    const googleSheets = google.sheets({
+        version: "v4", auth: client
+    })
+
+    // const spreadsheetId = "11Vo63U0mdIj_pTp0QQKZv7rtWXrycgJ9-uEE7Jfejqc"; // change this id for another spreadsheet
+    const spreadsheetId = "1feZLJJN4NDjAnqA8J5vHnVGrl9R91-NFGOqAW0gU5h4"; // change this id for another spreadsheet
+    // get metadata about the spreadsheet
+    const metadata = await googleSheets.spreadsheets.get({
+        auth, spreadsheetId
+    })
+
+    const rows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "Monday", majorDimension: "COLUMNS"
+    })
+    res.send(rows.data);
+})
+
+
+// test code ends here
 
 app.use('*', (req, res) => {
     res
