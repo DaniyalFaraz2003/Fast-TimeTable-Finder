9f7d545c49ebbd951b53437bcf9fe5e2517a0d9d
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-02-02 21:58:00 +0500

    ok

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { google } = require("googleapis");
+const fs = require('fs');
+const { authenticate } = require("google-auth-library");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -34,21 +36,63 @@ app.get("/", async (req, res) => {
         version: "v4", auth: client
     })
 
-    // const spreadsheetId = "11Vo63U0mdIj_pTp0QQKZv7rtWXrycgJ9-uEE7Jfejqc"; // change this id for another spreadsheet
-    const spreadsheetId = "1feZLJJN4NDjAnqA8J5vHnVGrl9R91-NFGOqAW0gU5h4"; // change this id for another spreadsheet
+    const spreadsheetIdLocal = "11Vo63U0mdIj_pTp0QQKZv7rtWXrycgJ9-uEE7Jfejqc"; // change this id for another spreadsheet
+    const spreadsheetIdCloud = "1feZLJJN4NDjAnqA8J5vHnVGrl9R91-NFGOqAW0gU5h4"; // change this id for another spreadsheet
     // get metadata about the spreadsheet
     const metadata = await googleSheets.spreadsheets.get({
-        auth, spreadsheetId
+        auth, spreadsheetId: spreadsheetIdCloud
     })
 
     const rows = await googleSheets.spreadsheets.values.get({
         auth,
-        spreadsheetId,
-        range: "Monday", majorDimension: "COLUMNS"
+        spreadsheetId: spreadsheetIdCloud,
+        range: "Monday",
     })
-    res.send(rows.data);
+
+
 })
 
+app.get("/download", async (req, res) => {
+    // Replace with your own credentials file path
+    const credentialsPath = 'key.json';
+
+    // Replace with the Google Sheets document ID
+    const sheetId = '11Vo63U0mdIj_pTp0QQKZv7rtWXrycgJ9-uEE7Jfejqc';
+
+    // Replace with the destination path on your computer
+    const destinationPath = './TimeTableFile/Time-Table.xlsx';
+
+    try {
+
+        const auth = new google.auth.GoogleAuth({
+            keyFile: credentialsPath,
+            scopes: 'https://www.googleapis.com/auth/drive.file',
+        });
+        const drive = google.drive({ version: 'v3', auth });
+
+        var dest = fs.createWriteStream(destinationPath);  // Please set the filename of the saved file.
+        drive.files.export(
+            { fileId: sheetId, alt: "media", mimeType: 'application/vnd.google-apps.spreadsheet' },
+            { responseType: "stream" },
+            (err,  data  ) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                data
+                    .on("end", () => console.log("Done."))
+                    .on("error", (err) => {
+                        console.log(err);
+                        return process.exit();
+                    })
+                    .pipe(dest);
+            }
+        );
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 // test code ends here
 
